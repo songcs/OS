b764b2e8efe2a3d958988fa4b2d10b2e9e1d4721
songcs <songcs@live.com>
2016-01-24 13:17:30 +0800

    用指针来代替汇编函数
    § *i = i & 0x0f;
    □ 但是这样会报错，因为类型不一样，此条编译成汇编是MOV [i], (i &
    0x0f），分不清[i]到底是BYTE,还是WORD或者是DWORD
    □ 正确方法是：
    ® char *p;
    *p = i & 0x0f;

--- OS/bootpack.c
+++ OS/bootpack.c
@@ -3,8 +3,10 @@ void write_mem8(int addr,int data);
 
 void HariMain(void){
 	int i;
+	char *p;
 	for(i=0xa0000;i<=0xaffff;i++){
-		write_mem8(i,i&0x0f); //write_mem8(i,15)是纯白色，现在这样是条纹状
+		p=i; //代入地址
+		*p=i&0x0f;//用指针来代替调用汇编函数
 	}
 	for(;;){
 		io_hlt();
